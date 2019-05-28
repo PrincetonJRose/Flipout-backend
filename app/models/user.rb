17b849967e2fdc17f2daea3a117c707e17a7b2fa
PrincetonJRose <decoyzpr@gmail.com>
2019-05-28 08:49:30 -0400

    Added game mode id column to user stats table.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
     has_many :user_stats
     has_many :stats, through: :user_stats
+
+    validates :username, presence: true
+    validates :username, uniqueness: true
 end
