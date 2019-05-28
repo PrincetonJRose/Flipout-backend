5b978ce5c3f502afe5337bc545c6333a10c41503
PrincetonJRose <decoyzpr@gmail.com>
2019-05-28 19:07:03 -0400

    Fixed saving users to db.

--- app/controllers/user_stats_controller.rb
+++ app/controllers/user_stats_controller.rb
@@ -1,2 +1,9 @@
 class UserStatsController < ApplicationController
+    def create
+        @user = User.find(params[:id])
+        @stats = Stat.all
+        @stats.each do |stat|
+            @new_stat = UserStat.create(user_id: @user.id, stat_id: stat.id, score: "0")
+        end
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,19 @@ class UsersController < ApplicationController
         @users = User.all
         render json: @users.to_json(include: :user_stats), status: :ok
     end
+
+    def create
+        @user = User.new(user_params)
+        if @user.save!
+            render json: @user.to_json(include: :user_stats), status: :ok
+        else
+            render status: :not_valid
+        end
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username)
+    end
 end
